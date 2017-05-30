b67a3c7a87bcd32c9df43bb94462a9e667848751
ckj3103 <ChetanJadhav123@users.noreply.github.com>
2017-05-30 11:52:24 +0530

    Add files via upload

---
+++ 8. UART.py
@@ -0,0 +1,30 @@
+'''
+import serial
+
+import os
+
+
+ser = serial.Serial("/dev/ttyAMA0")
+
+ser.baudrate=(9600)
+
+data = ser.readline(10)
+
+ser.write(data)
+
+ser.close()
+
+
+
+
+'''
+
+
+import serial
+
+port = serial.Serial ("/dev/ttyAMA0", baudrate-9600, timeout=0.2)
+
+while True:
+      rcv=port.redline()
+      if len(rcv)>10:
+          print(rcv)
